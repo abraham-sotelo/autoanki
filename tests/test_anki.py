b41d17ccf613c1fdcd9b9848a0c593231daf8ef6
Abraham <asoteloa@outlook.com>
2024-10-12 19:49:53 +0800

    anki.check_note_exists() added

--- tests/test_anki.py
+++ tests/test_anki.py
@@ -1,10 +1,10 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from src.anki import Anki
 
 class TestAnki(unittest.TestCase):
 
-  @patch('src.anki.requests.post')
+  @patch("src.anki.requests.post")
   def test_perform_action(self, mock_post):
     anki = Anki()
     test_cases = [
@@ -26,6 +26,30 @@ class TestAnki(unittest.TestCase):
         expectedResult = case["expectedResult"]
         mock_post.return_value.json.return_value = expectedResult
         result = anki.perform_action(action, params)
+        self.assertEqual(result, expectedResult)
+
+
+  @patch("src.anki.requests.post")
+  def test_check_note_exists(self, mock_post):
+    anki = Anki()
+    test_cases = [
+      {"msg": "Test with correct deck, note and fields",
+       "deck": "deck", "note": "note", "fields": ["field1", "field2"], "expectedResult": True},
+      {"msg": "Test with wrong deck",
+       "deck": "wrongDeck", "note": "note", "fields": ["field1", "field2"], "expectedResult": False},
+      {"msg": "Test with wrong note",
+       "deck": "deck", "note": "wrongNote", "fields": ["field1", "field2"], "expectedResult": False},
+      {"msg": "Test with wrong fields",
+       "deck": "deck", "note": "note", "fields": ["wrongField1", "wrongField2"], "expectedResult": False},
+    ]
+    for case in test_cases:
+      with self.subTest(msg = f"{case["msg"]}"):
+        deck = case["deck"]
+        note = case["note"]
+        fields = case["fields"]
+        expectedResult = case["expectedResult"]
+        mock_post.return_value.json.return_value = {"result": ["note1", "note2"]} if expectedResult else {"result": []}
+        result = anki.check_note_exists(deck, note, fields)
         self.assertEqual(result, expectedResult)
 
 
